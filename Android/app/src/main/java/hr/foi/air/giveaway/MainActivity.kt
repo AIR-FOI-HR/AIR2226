611647dec43441870cadd9c21be8261dcb156117
Nikola Biškup <nikolabiskup101@gmail.com>
2024-01-01 18:35:19 +0100

    Added support for user registration

--- Android/app/src/main/java/hr/foi/air/giveaway/MainActivity.kt
+++ Android/app/src/main/java/hr/foi/air/giveaway/MainActivity.kt
@@ -1,6 +1,5 @@
 package hr.foi.air.giveaway
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,10 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import hr.foi.air.giveaway.navigation.components.EntryPage
+import hr.foi.air.giveaway.navigation.components.registration.PostRegistration
+import hr.foi.air.giveaway.navigation.components.registration.RegistrationPage
 import hr.foi.air.giveaway.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,15 +42,26 @@ class MainActivity : ComponentActivity() {
 
                         }
                         composable("register") {
-
+                            RegistrationPage(
+                                onSuccessfulRegistration = { newUsername ->
+                                    navController.navigate("register/$newUsername/notice")
+                                }
+                            )
+                        }
+                        composable(
+                            "register/{username}/notice",
+                            arguments = listOf(navArgument("username") { type = NavType.StringType })
+                        ) { backStackEntry ->
+                            PostRegistration(
+                                newUsername = backStackEntry.arguments?.getString("username") ?: "?",
+                                onNoticeUnderstood = {
+                                    repeat(2) { navController.popBackStack() }
+                                }
+                            )
                         }
                     }
                 }
-
             }
-
         }
-
-
     }
 }
---
+++ Android/app/src/main/java/hr/foi/air/giveaway/entities/RegistrationBody.kt
@@ -0,0 +1,10 @@
+package hr.foi.air.giveaway.entities
+
+data class RegistrationBody(
+    val first_name: String,
+    val last_name: String,
+    val username: String,
+    val email: String,
+    val password: String,
+    val role: String,
+)
